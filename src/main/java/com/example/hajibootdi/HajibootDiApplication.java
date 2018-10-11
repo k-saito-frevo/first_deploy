8f6c29fdfde84f5729dbdb7f5dc6087ef665c5ba
k-saito-frevo <k.saito@frevo-works.co.jp>
2018-10-11 15:29:34 +0900

    最初のコミット

---
+++ src/main/java/com/example/hajibootdi/HajibootDiApplication.java
@@ -0,0 +1,14 @@
+
+package com.example.hajibootdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HajibootDiApplication {
+
+	public static void main(String[] args) {
+		System.out.print("test");
+		SpringApplication.run(HajibootDiApplication.class, args);
+	}
+}
